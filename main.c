3a6d4684a820535cdc0e8b0522b7f6ba0b1fee62
Ярослав Семченко <ukrkyi@gmail.com>
2018-02-11 16:24:25 +0200

    Added button polling
    Signed-off-by: Ярослав Семченко <ukrkyi@gmail.com>

--- main.c
+++ main.c
@@ -3,11 +3,22 @@
 int main(void)
 {
 	// Port C led setup
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPBEN;
 	GPIOC->CRH |= GPIO_CRH_MODE13_1;
+
+	// Switch port setup
+	GPIOB->CRH = (GPIOB->CRH & ~GPIO_CRH_CNF8) | (0x2 << GPIO_CRH_CNF8_Pos);
+	GPIOB->ODR |= GPIO_ODR_ODR8;
+
 	while (1) {
-		GPIOC->ODR ^= GPIO_ODR_ODR13;
-		for (volatile int i = 0; i < 1000000; i++);
+		if (GPIOB->IDR & GPIO_IDR_IDR8)
+		{
+			GPIOC->BSRR = GPIO_BSRR_BS13;
+		}
+		else
+		{
+			GPIOC->BSRR = GPIO_BSRR_BR13;
+		}
 	}
 	return 0;
 }
