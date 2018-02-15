6b437fbd1288b659b1e7720ac07750e51bc5a2c4
Ярослав Семченко <ukrkyi@gmail.com>
2018-02-15 13:59:41 +0200

    Button press is now handled via EXTI
    Signed-off-by: Ярослав Семченко <ukrkyi@gmail.com>

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stm32f1xx.h>
 
+void EXTI9_5_IRQHandler(void)
+{
+	if (GPIOB->IDR & GPIO_IDR_IDR8)
+	{
+		GPIOC->BSRR = GPIO_BSRR_BS13;
+	}
+	else
+	{
+		GPIOC->BSRR = GPIO_BSRR_BR13;
+	}
+}
+
 int main(void)
 {
 	// Port C led setup
@@ -9,16 +21,16 @@ int main(void)
 	// Switch port setup
 	GPIOB->CRH = (GPIOB->CRH & ~GPIO_CRH_CNF8) | (0x2 << GPIO_CRH_CNF8_Pos);
 	GPIOB->ODR |= GPIO_ODR_ODR8;
+	// Interrupt enable
+	EXTI->IMR |= EXTI_IMR_IM8;
+	EXTI->RTSR |= EXTI_RTSR_RT8;
+	EXTI->FTSR |= EXTI_FTSR_FT8;
+
+	// NVIC Configuration
+	NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 	while (1) {
-		if (GPIOB->IDR & GPIO_IDR_IDR8)
-		{
-			GPIOC->BSRR = GPIO_BSRR_BS13;
-		}
-		else
-		{
-			GPIOC->BSRR = GPIO_BSRR_BR13;
-		}
+		;
 	}
 	return 0;
 }
