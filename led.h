ef0ee1fb59b9c57f3b81e5d6897d9d7c46c0fe7c
Ярослав Семченко <ukrkyi@gmail.com>
2018-02-25 01:53:24 +0200

    Added RGB LED PWM
    Signed-off-by: Ярослав Семченко <ukrkyi@gmail.com>

--- led.h
+++ led.h
@@ -6,4 +6,7 @@ extern void led_on();
 extern void led_off();
 extern void led_toggle();
 
+extern void led_rgb_init();
+extern void led_rgb_setcolor(unsigned r, unsigned g, unsigned b);
+
 #endif // LED_H
--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-static int position;
+static int max = 100;
 static char buff[100];
 
 void button_state_change_callback()
@@ -23,28 +23,51 @@ void button_state_change_callback()
 
 void encoder_step_callback(bool backward, int pos)
 {
-	if (backward)
-		uart_send("-");
-	else
-		uart_send("+");
-	position = pos;
+	if (backward) {
+		if (max > 0)
+			max--;
+	} else if (max < 100)
+		max++;
 }
 
 int main(void)
 {
+	int i = 0, j = 100;
+	bool up = false;
+
 	set_system_clock();
 
 	led_init();
 	led_off();
+	led_rgb_init();
+	led_rgb_setcolor(100, 100, 100);
 	encoder_init();
 	uart_init();
 
 	uart_send("Hello!\r\n");
 
 	while (1) {
-		snprintf(buff, 10, "%" PRIu32 "\r\n", position);
-		uart_send(buff);
-		for (volatile int i = 0; i < 10000000; i++);
+		switch (i) {
+		case 0: led_rgb_setcolor(j, max, max); break;
+		case 1: led_rgb_setcolor(max, j, max); break;
+		case 2: led_rgb_setcolor(max, max, j); break;
+		case 3: led_rgb_setcolor(j, j, max); break;
+		case 4: led_rgb_setcolor(j, max, j); break;
+		case 5: led_rgb_setcolor(max, j, j); break;
+		case 6: led_rgb_setcolor(j, j, j); break;
+		}
+		if (up) {
+			j++;
+			if (j >= max) {
+				up = false;
+				i = (i + 1) % 7;
+			}
+		} else {
+			j--;
+			if (j == 0)
+				up = true;
+		}
+		for (volatile k = 0; k < 200000; k++);
 	}
 	return 0;
 }
